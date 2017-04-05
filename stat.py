16c660638bff2459d753304689502916adb3d4f6
wkl7123 <wkl7123@gmail.com>
2017-04-06 02:24:01 +0800

    Update stat.py

--- stat.py
+++ stat.py
@@ -1,4 +1,21 @@
 import os
-domainList=map(lambda x:x[0:-1], os.popen('plesk bin domain --list').readlines())
-subscriptionList=map(lambda x:x[0:-1], os.popen('plesk bin subscription --list').readlines())
 class Subscription:
+  domainList=[]
+  loginName=''
+  ftpLoginName=''
+  servicePlan=''
+  contactName=''
+  email=''
+  hitNum=0
+  def countHit(self, domainHitList):
+    #
+  def isOverHit(self):
+    #
+  def sendEmail(self):
+    #
+def main():
+  domainList=map(lambda x:x[0:-1], os.popen('plesk bin domain --list').readlines())
+  domainHitList={}
+  subscriptionList=map(lambda x:x[0:-1], os.popen('plesk bin subscription --list').readlines())
+  servicePlanHitDic={'Standard':10**4, 'Professional':10**5, 'Business':10**6}
+  adminEmail=os.popen("plesk bin user --info admin | grep Email | awk '{print $2}'").readline()[0:-1]
